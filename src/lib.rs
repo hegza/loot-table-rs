4cc648d24697694bc7b87a36f991bc7041899bb7
Henri Lunnikivi <henri.lunnikivi@gmail.com>
2017-11-29 10:00:10 +0200

    Initial release

---
+++ src/lib.rs
@@ -0,0 +1,50 @@
+#![allow(dead_code)]
+extern crate rand;
+mod tests;
+
+use rand::{Rng, SeedableRng, StdRng};
+
+trait Item: Eq + Clone {}
+
+struct LootTable<T: Item> {
+    rng: StdRng,
+    sum_of_chances: u32,
+    chances: Vec<u32>,
+    items: Vec<T>,
+}
+
+impl<T> LootTable<T>
+where
+    T: Item,
+{
+    fn from_equal_chance(items: &[T], seed: &[usize]) -> LootTable<T> {
+        let item_count = items.iter().count();
+        let chances = vec![1 as u32; item_count];
+        LootTable {
+            rng: SeedableRng::from_seed(seed),
+            sum_of_chances: item_count as u32,
+            chances: chances,
+            items: items.to_vec(),
+        }
+    }
+    fn chance_for(&self, item: &T) -> f32 {
+        let position = self.items.iter().position(|i| i == item);
+        match position {
+            Some(idx) => self.chances[idx] as f32 / self.sum_of_chances as f32,
+            None => 0.,
+        }
+    }
+    fn roll(&mut self) -> &T {
+        let choice = self.rng.gen::<u32>() % self.sum_of_chances;
+
+        let mut accumulator = 0;
+        for (idx, item) in self.items.iter().enumerate() {
+            accumulator += self.chances[idx];
+            if accumulator > choice {
+                return item;
+            }
+        }
+
+        panic!("loot table is empty");
+    }
+}
---
+++ src/tests.rs
@@ -0,0 +1,22 @@
+#[cfg(test)]
+use Item;
+use super::*;
+
+#[derive(Clone, PartialEq, Eq)]
+struct TestItem(i32);
+impl Item for TestItem {}
+
+#[test]
+fn it_works() {
+    let items = vec![TestItem(0), TestItem(1), TestItem(2)];
+    let table = LootTable::from_equal_chance(&items, &[1, 2, 3, 4]);
+
+    let chance_for_0 = table.chance_for(&items[0]);
+    let chance_for_1 = table.chance_for(&items[1]);
+    let chance_for_2 = table.chance_for(&items[2]);
+
+    const EPSILON: f32 = 0.001;
+    assert!((chance_for_0 - (1. / 3.)).abs() < EPSILON);
+    assert!((chance_for_1 - (1. / 3.)).abs() < EPSILON);
+    assert!((chance_for_2 - (1. / 3.)).abs() < EPSILON);
+}
